99c94e7bb04b73be829416ddbd80f3d1def13a0c
shestopalovmatvey <shestopalov.myu@edu.spbstu.ru>
2023-03-06 23:48:05 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,112 @@
+let arrOfBtn = document.querySelectorAll('.calc__button');
+let output = document.querySelector('.inputNum')
+let currentStr = '';
+let currentSum = '';
+let prevSum = '';
+let currentOper = '';
+let s = "";
+
+let oper = {
+    isPlus: false,
+    isMinus: false,
+    isMul: false,
+    isDiv: false,
+}
+
+let setNewValue = () => {
+    prevSum = currentSum;
+    currentStr = '';
+    currentSum = '';
+    output.textContent = '0';
+}
+
+let resetOper = () => {
+    for (let key in oper) {
+        oper[key] = false;
+    }
+}
+
+let resetValue = () => {
+    output.innerText = '0';
+    currentStr = '';
+    currentSum = '';                
+    prevSum = '';
+    resetOper();
+}
+
+let kindOfOperation = (oper, currentSum, prevSum) => {
+    switch (oper) {
+        case 'isPlus':
+            return +currentSum + +prevSum;
+        case 'isMinus':
+            return +prevSum - +currentSum;
+        case 'isMul':
+            return +currentSum * +prevSum;
+        case 'isDiv':
+            return (+prevSum / +currentSum).toFixed(4)
+    }
+}
+
+let elementIsSign = (element) => {
+    let arr = ['+', '−', '=', '×', '÷', '←', 'C']
+    return arr.includes(element)
+}
+
+for (let i = 0; i < arrOfBtn.length; i++) {
+    arrOfBtn[i].addEventListener('click', (evt) => {
+        let isSign = elementIsSign(evt.target.innerText)
+        if (evt.target.innerText === 'C') {
+            resetValue();                  
+        }
+        if (!isSign) {
+            currentStr += evt.target.innerText;
+            currentSum = currentStr;
+            output.innerText = currentStr;
+        }
+        if (currentSum != 0 && isSign) {
+            switch (evt.target.innerText) {
+                case 'C':
+                    resetValue();
+                    break;
+                case '←':
+                    if (currentStr.length - 1 === 0) {
+                        resetValue();
+                    } else {
+                        s = currentStr.slice(0, currentStr.length - 1);
+                        console.log(s)
+                        currentStr = s;
+                        currentSum = currentStr;
+                        output.innerText = currentStr;
+                    }
+                    break
+                case '+':
+                    setNewValue();
+                    oper.isPlus = true;
+                    break;
+                case '−':
+                    setNewValue();
+                    oper.isMinus = true;
+                    break;
+                case '×':
+                    setNewValue();
+                    oper.isMul = true;
+                    break
+                case '÷':
+                    setNewValue();
+                    oper.isDiv = true;
+                    break
+                case '=':
+                    for (let key in oper) {
+                        if (oper[key]) {
+                            currentOper = key;
+                        }
+                    }
+                    currentSum = kindOfOperation(currentOper, currentSum, prevSum);
+                    currentStr = currentSum;
+                    output.textContent = currentStr;
+                    resetOper();
+                    break;
+            }
+        }
+    })
+}
